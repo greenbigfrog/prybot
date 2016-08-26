15bbf8626b9b96153f439fa79ffd956d40335a8e
Jonathan <greenbigfrog@gmail.com>
2016-08-26 22:06:56 +0200

    remove clutter

--- main.rb
+++ main.rb
@@ -1,10 +1,5 @@
-if RUBY_VERSION < '2.2.0'
-  puts 'Please update your ruby!'
-  exit
-end
 
 # require 'bundler/setup'
-
 require 'pry'
 
 require 'dotenv'
@@ -14,9 +9,8 @@ require 'discordrb'
 bot = Discordrb::Commands::CommandBot.new token: ENV['TOKEN'], application_id: ENV['APPID'], prefix: '!', advanced_functionality: false, help_command: false, debug: true #, log_mode: :debug
 
 bot.ready do
+  bot.game = 'private'
   binding.pry
 end
 
-bot.run :async
-bot.game = 'private'
-bot.sync
+bot.run
