b49b7d00164c6afe72352584caa9d8a89cf6c9cc
Jonathan B <greenbigfrog@gmail.com>
2016-08-28 20:19:15 +0200

    Push user branch/settings

--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ require 'dotenv'
 Dotenv.load
 
 require 'discordrb'
-bot = Discordrb::Commands::CommandBot.new token: ENV['TOKEN'], application_id: ENV['APPID'], prefix: '!', advanced_functionality: false, help_command: false, debug: true #, log_mode: :debug
+bot = Discordrb::Commands::CommandBot.new token: ENV['TOKEN'], type: :user, prefix: '!', advanced_functionality: false, help_command: false, debug: true #, log_mode: :debug
 
 bot.ready do
   bot.game = 'private'
